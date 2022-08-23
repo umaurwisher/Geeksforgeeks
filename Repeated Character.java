71ef908571242eef7269e08ca23267923eba612c
umaurwisher <111058124+umaurwisher@users.noreply.github.com>
2022-08-23 13:29:41 +0530

    Create Repeated Character.java

---
+++ Repeated Character.java
@@ -0,0 +1,58 @@
+/*Given a string consisting of lowercase english alphabets. Find the repeated character present first in the string.
+
+Example 1:
+
+Input:
+S = "geeksforgeeks"
+Output: g
+Explanation: g, e, k and s are the repeating
+characters. Out of these, g occurs first. */
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+            
+            Solution ob = new Solution();
+            char res = ob.firstRep(s);
+            if (res == '#')
+                System.out.println(-1);
+            else
+                System.out.println(res);
+             
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    char firstRep(String S)
+    {
+        // your code here
+        int i,j,count=0;
+        int n=S.length();
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+            if(S.charAt(i)==S.charAt(j))
+            
+                return S.charAt(i);
+            }
+        }
+        return '#';
+        
+    }
+}
