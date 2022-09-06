3a3a6922efd0a819e39ce36a7a50f8181a4c782d
umaurwisher <111058124+umaurwisher@users.noreply.github.com>
2022-09-06 23:04:49 +0530

    Create Missing number in array.java

---
+++ Missing number in array.java
@@ -0,0 +1,57 @@
+/*
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.MissingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+       int sum=0,sum1=0,i;
+       for(i=1;i<=n;i++)
+       {
+           sum=sum+i;
+       }
+       for(i=0;i<n-1;i++)
+       {
+           sum1=sum1+array[i];
+       }
+       return sum-sum1;        
+    }
+}
